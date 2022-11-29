487b799b2242810c5646401bd641f035378c52f8
ssearobber <ssearobber@naver.com>
2022-11-29 16:12:13 +0900

    make it better

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  const port = process.env.PORT || '3000';
+  const port = process.env.PORT || 3000;
   await app.listen(port);
 }
 bootstrap();
